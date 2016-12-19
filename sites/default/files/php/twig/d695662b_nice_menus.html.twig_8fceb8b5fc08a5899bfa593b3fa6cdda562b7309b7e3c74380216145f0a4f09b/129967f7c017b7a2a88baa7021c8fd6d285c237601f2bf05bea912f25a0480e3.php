97e7139930e8f712c53c4c9313858955c9e9056d
kadantsew <o.kadantsev@internetdevels.com>
2016-12-19 17:49:33 +0200

    foundation

---
+++ sites/default/files/php/twig/d695662b_nice_menus.html.twig_8fceb8b5fc08a5899bfa593b3fa6cdda562b7309b7e3c74380216145f0a4f09b/129967f7c017b7a2a88baa7021c8fd6d285c237601f2bf05bea912f25a0480e3.php
@@ -0,0 +1,61 @@
+<?php
+
+/* modules/contrib/nice_menus/templates/nice_menus.html.twig */
+class __TwigTemplate_170b9531466ef12c1cdad2cb0134158782e8ff74553c50ac96be66bf0a0014a5 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["menu_output"]) ? $context["menu_output"] : null), "html", null, true));
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/nice_menus/templates/nice_menus.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+}
+/* {{ menu_output }}*/
